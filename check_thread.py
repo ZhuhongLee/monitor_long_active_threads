bffd764186b732bdb5dcd266e87ef5a81d78eddb
zhuhong.Lee <lizhuhongno1@163.com>
2021-01-04 10:31:41 +0800

    add more detail

--- check_thread.py
+++ check_thread.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/python3.7
+
 import pymysql
 import time
 import json
@@ -8,15 +9,19 @@ import argparse
 from settings import DBLIST_INFO
 
 
-def connectdb(args, **dblist):
-    # 获取执行时间大于指定时间args.t的SQL
+def connectdb(**dblist):
+    # 获取执行时间大于5s的SQL，where time>5
     sql = '''
         select
             id,
             user,
             host,
+            "%s" as project,
             db,
             time,
+            date_add(now(), INTERVAL -time SECOND)	exec_stime,
+            command,
+            state,
             info,
             sha(
                 concat(
@@ -30,13 +35,13 @@ def connectdb(args, **dblist):
             information_schema.`processlist`
         where
             info <> ''
-        and time > %s
+        and time >= %s
         and user <>'mysqldump'
-     ''' % (args.t)
+     ''' % (dblist['project'], args.t)
 
     try:
         conn = pymysql.connect(host=dblist['host'], user=dblist['username'], passwd=dblist['password'],
-                               port=dblist['port'],
+                               port=dblist['port'], charset='utf8',
                                cursorclass=pymysql.cursors.DictCursor)
         cursor = conn.cursor()
         cursor.execute(sql)
@@ -59,8 +64,9 @@ def checkthred(plist, result):
         if not plist:
             for v_sql in result:
                 plist.append(v_sql)
-                ding_md = '# <font face=\"微软雅黑\">发现SLOW SQL</font> \n **线程ID:**  <font color=\"#000080\">%s</font><br /> \n \n  **用  户:**  <font color=\"#000080\">%s</font><br /> \n \n **主  机:**  <font color=\"#000080\">%s</font><br /> \n \n**DBNAME:**  <font color=\"#000080\">%s</font><br /> \n \n**执行时间(s):**  <font color=\"#000080\">%s</font><br /> \n \n**SQL:**  <font color=\"#000080\">%s</font><br /> \n' % (
-                    v_sql['id'], v_sql['user'], v_sql['host'], v_sql['db'], v_sql['time'], v_sql['info'])
+                ding_md = '# <font face=\"微软雅黑\">发现SLOW SQL</font> \n **InstanceName:**  <font font-weight=700 color=\"#000080\">%s</font><br /> \n \n **ThreadID:**  <font color=\"#000080\">%s</font><br /> \n \n  **User:**  <font color=\"#000080\">%s</font><br /> \n \n **Host:**  <font color=\"#000080\">%s</font><br /> \n \n**DB:**  <font color=\"#000080\">%s</font><br /> \n \n **Command:**  <font color=\"#000080\">%s</font><br /> \n \n **State:**  <font color=\"#000080\">%s</font><br /> \n \n **ExecStartTime:**  <font color=\"#000080\">%s</font><br /> \n \n **ExecTime(s):**  <font color=\"#f90202\">%s</font><br /> \n \n**SQL:**  <font color=\"#000080\">%s</font><br /> \n' % (
+                    v_sql['project'], v_sql['id'], v_sql['user'], v_sql['host'], v_sql['db'], v_sql['command'],
+                    v_sql['state'], v_sql['exec_stime'], v_sql['time'], v_sql['info'])
                 dingding_robot(ding_md)
         else:
             for active_session in result:
@@ -68,10 +74,12 @@ def checkthred(plist, result):
                     if active_session['vsha'] != plist[i]['vsha']:
                         if i == len(plist) - 1:
                             plist.append(active_session)
-                            ding_md = '# <font face=\"微软雅黑\">发现SLOW SQL</font> \n **线程ID:**  <font color=\"#000080\">%s</font><br /> \n \n  **用  户:**  <font color=\"#000080\">%s</font><br /> \n \n **主  机:**  <font color=\"#000080\">%s</font><br /> \n \n**DBNAME:**  <font color=\"#000080\">%s</font><br /> \n \n**执行时间(s):**  <font color=\"#000080\">%s</font><br /> \n \n**SQL:**  <font color=\"#000080\">%s</font><br /> \n' % (
-                                active_session['id'], active_session['user'], active_session['host'],
-                                active_session['db'],
-                                active_session['time'], active_session['info'])
+                            ding_md = '# <font face=\"微软雅黑\">发现SLOW SQL</font> \n **InstanceName:**  <font font-weight=700 color=\"#000080\">%s</font><br /> \n \n **ThreadID:**  <font color=\"#000080\">%s</font><br /> \n \n  **User:**  <font color=\"#000080\">%s</font><br /> \n \n **Host:**  <font color=\"#000080\">%s</font><br /> \n \n**DB:**  <font color=\"#000080\">%s</font><br /> \n \n **Command:**  <font color=\"#000080\">%s</font><br /> \n \n **State:**  <font color=\"#000080\">%s</font><br /> \n \n **ExecStartTime:**  <font color=\"#000080\">%s</font><br /> \n \n **ExecTime(s):**  <font color=\"#f90202\">%s</font><br /> \n \n**SQL:**  <font color=\"#000080\">%s</font><br /> \n' % (
+                                active_session['project'], active_session['id'], active_session['user'],
+                                active_session['host'], active_session['db'],
+                                active_session['command'],
+                                active_session['state'], active_session['exec_stime'], active_session['time'],
+                                active_session['info'])
                             dingding_robot(ding_md)
                         else:
                             continue
@@ -82,9 +90,11 @@ def checkthred(plist, result):
         # 判断session是否执行完毕，若是，则从列表中删除此sql，并钉钉推送执行完成，若否，则不做处理
         if not result:
             for index, plist_session in enumerate(plist):
-                ding_md = '# <font face=\"微软雅黑\">SLOW SQL执行完成</font> \n **线程ID:**  <font color=\"#000080\">%s</font><br /> \n \n  **用  户:**  <font color=\"#000080\">%s</font><br /> \n \n **主  机:**  <font color=\"#000080\">%s</font><br /> \n \n**DBNAME:**  <font color=\"#000080\">%s</font><br /> \n \n**执行时间(s):**  <font color=\"#E8441B\">%s</font><br /> \n \n**SQL:**  <font color=\"#000080\">%s</font><br /> \n' % (
-                    plist_session['id'], plist_session['user'], plist_session['host'], plist_session['db'],
-                    plist_session['time'], plist_session['info'])
+                ding_md = '# <font face=\"微软雅黑\">SLOW SQL执行完成</font> \n **InstanceName:**  <font font-weight=700 color=\"#000080\">%s</font><br /> \n \n **ThreadID:**  <font color=\"#000080\">%s</font><br /> \n \n  **User:**  <font color=\"#000080\">%s</font><br /> \n \n **Host:**  <font color=\"#000080\">%s</font><br /> \n \n**DB:**  <font color=\"#000080\">%s</font><br /> \n \n **Command:**  <font color=\"#000080\">%s</font><br /> \n \n **State:**  <font color=\"#000080\">%s</font><br /> \n \n **ExecStartTime:**  <font color=\"#000080\">%s</font><br /> \n \n **ExecTime(s):**  <font color=\"#32CD32\">%s</font><br /> \n \n**SQL:**  <font color=\"#000080\">%s</font><br /> \n' % (
+                    plist_session['project'], plist_session['id'], plist_session['user'], plist_session['host'],
+                    plist_session['db'],
+                    plist_session['command'],
+                    plist_session['state'], plist_session['exec_stime'], plist_session['time'], plist_session['info'])
                 dingding_robot(ding_md)
                 plist.pop(index)
 
@@ -95,9 +105,12 @@ def checkthred(plist, result):
                         break
                     else:
                         if plist_session['vsha'] != result[i]['vsha'] and i == len(result) - 1:
-                            ding_md = '# <font face=\"微软雅黑\">SLOW SQL执行完成</font> \n **线程ID:**  <font color=\"#000080\">%s</font><br /> \n \n  **用  户:**  <font color=\"#000080\">%s</font><br /> \n \n **主  机:**  <font color=\"#000080\">%s</font><br /> \n \n**DBNAME:**  <font color=\"#000080\">%s</font><br /> \n \n**执行时间(s):**  <font color=\"#E8441B\">%s</font><br /> \n \n**SQL:**  <font color=\"#000080\">%s</font><br /> \n' % (
-                                plist_session['id'], plist_session['user'], plist_session['host'], plist_session['db'],
-                                plist_session['time'], plist_session['info'])
+                            ding_md = '# <font face=\"微软雅黑\">SLOW SQL执行完成</font> \n **InstanceName:**  <font font-weight=700 color=\"#000080\">%s</font><br /> \n \n **ThreadID:**  <font color=\"#000080\">%s</font><br /> \n \n  **User:**  <font color=\"#000080\">%s</font><br /> \n \n **Host:**  <font color=\"#000080\">%s</font><br /> \n \n**DB:**  <font color=\"#000080\">%s</font><br /> \n \n **Command:**  <font color=\"#000080\">%s</font><br /> \n \n **State:**  <font color=\"#000080\">%s</font><br /> \n \n **ExecStartTime:**  <font color=\"#000080\">%s</font><br /> \n \n **ExecTime(s):**  <font color=\"#32CD32\">%s</font><br /> \n \n**SQL:**  <font color=\"#000080\">%s</font><br /> \n' % (
+                                plist_session['project'], plist_session['id'], plist_session['user'],
+                                plist_session['host'], plist_session['db'],
+                                plist_session['command'],
+                                plist_session['state'], plist_session['exec_stime'], plist_session['time'],
+                                plist_session['info'])
                             dingding_robot(ding_md)
                             plist.pop(index)
                         else:
@@ -111,6 +124,7 @@ def checkthred(plist, result):
 
 
 def dingding_robot(content):
+    # 替换钉钉 access_token
     webhook = "https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     headers = {'content-type': 'application/json'}
     data = {"msgtype": "markdown", "markdown": {"title": "SLOW SQL监控", "text": content}}
@@ -119,11 +133,11 @@ def dingding_robot(content):
     return (r.text)
 
 
-def main(args):
+def main():
     global result
     result = []
     for db in DBLIST_INFO:
-        result = result + connectdb(args, **db)
+        result = result + connectdb(**db)
 
     checkthred(plist, result)
 
@@ -139,7 +153,8 @@ if __name__ == "__main__":
 
     # main()
 
-    schedule.every(2).seconds.do(main, args)         ## 定时任务，每两秒执行一次
+    ## 定时任务，每秒执行一次,sleep 一秒
+    schedule.every(1).seconds.do(main)
     while True:
         schedule.run_pending()
         time.sleep(1)
--- settings.py
+++ settings.py
@@ -7,5 +7,5 @@
 '''
 
 
-DBLIST_INFO = [{'host': '192.168.211.1', 'port': 3306, 'username': 'root', 'password': 'root123'},
-               {'host': '192.168.211.1', 'port': 3307, 'username': 'root', 'password': 'root123'}]
+DBLIST_INFO = [{'host': '192.168.211.1', 'port': 3306, 'username': 'root', 'password': 'root123','project': '测试环境CRM主实例'},
+               {'host': '192.168.211.1', 'port': 3307, 'username': 'root', 'password': 'root123','project': '测试环境CMS主实例'}]
